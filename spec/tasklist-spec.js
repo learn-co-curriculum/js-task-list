f4ea5d03c8442156f4788a50d8f9f2d511f29f3c
Rose Weixel <rose.weixel@gmail.com>
2015-01-22 15:08:45 -0500

    complete some tests

--- spec/tasklist-spec.js
+++ spec/tasklist-spec.js
@@ -1,59 +1,41 @@
 'use-strict';
 
-describe('Validating User Input', function() {
+describe('Adding and Removing Tasks', function() {
   beforeEach(function() {
     setFixtures('<div id="wrapper"><h1>my to do list</h1>  <form id="add_task" action="#" method="post"><label for="task_name">Add a new task:</label><input type="text" id="task_name" name="task_name" placeholder="taskname"><input type="submit" value="(+) add"></form><!-- #add_task --><form id="remove_task" action="#" method="post"><label for="task_num">Remove task:</label><input type="text" id="task_num" name="task_num" placeholder="number"><input type="submit" value="(-) remove"></form><!-- #remove_task --><ul id="task_list"></ul><!-- #task_list --></div><!-- #wrapper -->');
 
-    // displayList();
-    // $('form').submit(function(e){ e.preventDefault(); });
-    // var taskName = '', // task name
-    //     taskNum = '', //task number
-    //     taskList = []; //task list
-        
-    // //DOM SEL
-    // var taskNameInput = $('#task_name'),
-    //     taskNumInput = $('#task_num'),
-    //     taskListArea = $('#task_list');
-        
-
-    // //DISPLAY LIST FUNCTION
-    // function displayList() {
-    
-    //     taskListArea.html(''); // blank out the list
-    
-    //     $.each(taskList, function(i, value){ //each iterates over all elements in the array one by one.
-            
-    //         taskListArea.append('<li>'+(i + 1)+'. <span class="name">'+value+'</span></li>'); //add back each name in the list.
-    //         console.log(taskList[i]);
-            
-    //     });
-    //     console.log('----------');
-    
-    // }
-    
-    // //EVENT ADD_task FORM SUBMIT 
-    // $('#add_task').submit(function(){
-    
-    //     taskName = taskNameInput.val(); //store the currently typed task name value
-        
-    //     $('#task_name').val(''); //clear input
-        
-    //     if(taskName !== ''){ //make sure field is not empty!
-            
-    //         taskList.push(taskName); //add taskname to the list array
-    //         displayList();
-    //     }
-        
-    //     return false; //stops form from processing (prevent page reload)
-    // });
+    $('form').submit(function(e){ e.preventDefault(); });
+    $('#add_task').submit(addTask);
+    $('#remove_task').submit(removeTask);
+    displayList();
   });
 
   it('#add_task should add a task to ul#task_list', function() {
     $('#task_name').val('buy groceries');
-    //$('input[type="submit"]').last().click();
     $('#add_task').submit();
-    debugger;
+
     expect($('#task_list').children().first().text()).toBe('1. buy groceries');
+  });
+
+  it('#remove_task should remove the specified task from ul#task_list', function() {
+    $('#task_name').val('do laundry');
+    $('#add_task').submit();
+    $('#task_num').val('1');
+    $('#remove_task').submit();
+
+    expect($('#task_list').children().text()).not.toContain('buy groceries');
+  });
+
+  it('#remove_task should adjust the numbering of the remaining list items', function() {
+    expect($('#task_list').children().first().text()).toBe('1. do laundry');
+  });
+
+  it('#remove_task should alert the user with an error message if the input is not a number', function() {
+    $('#task_num').val('cat');
+    $('#remove_task').submit();
+    
+    // this is not the right test
+    //expect(alert('blah'));
   });
 
 });
